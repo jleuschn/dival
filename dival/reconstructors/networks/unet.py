297812cd581527cdb7cd16dcdb32147556347c57
Johannes Leuschner <jleuschn@uni-bremen.de>
2020-09-21 09:43:09 +0200

    fix unet in case of use_norm=False

--- dival/reconstructors/networks/unet.py
+++ dival/reconstructors/networks/unet.py
@@ -22,7 +22,7 @@ class UNet(nn.Module):
         self.use_sigmoid = use_sigmoid
         self.down = nn.ModuleList()
         self.up = nn.ModuleList()
-        self.inc = InBlock(in_ch, channels[0])
+        self.inc = InBlock(in_ch, channels[0], use_norm=use_norm)
         for i in range(1, self.scales):
             self.down.append(DownBlock(in_ch=channels[i - 1],
                                        out_ch=channels[i],
