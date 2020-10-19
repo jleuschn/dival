515d5d227e3333afe9255eb5a41d8eda750e6165
Johannes Leuschner <jleuschn@uni-bremen.de>
2020-10-19 17:27:32 +0200

    fix imports of StandardLearnedReconstructor

--- dival/reconstructors/iradonmap_reconstructor.py
+++ dival/reconstructors/iradonmap_reconstructor.py
@@ -2,7 +2,8 @@
 import torch.nn as nn
 from copy import deepcopy
 
-from dival.reconstructors.StandardLearnedReconstructor import StandardLearnedReconstructor
+from dival.reconstructors.standard_learned_reconstructor import (
+    StandardLearnedReconstructor)
 from dival.reconstructors.networks.iradonmap import get_iradonmap_model
 
 
